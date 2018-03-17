44fa271d4343cd14d9182551ec2ef404137b2cf7
salehrabbi <33930388+salehrabbi@users.noreply.github.com>
2018-03-17 15:40:13 +0600

    pc version

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,13 @@ $(function () {
             html: true
         })
 })
-$("#play-list-class").click(function(){
+$("#play-list-class").click(function () {
     $(".play-list").toggleClass("active");
+});
+
+$(function() {
+
+	$('#diagram-id-1').circleDiagram();
+	$('#diagram-id-2').circleDiagram();
+	$('#diagram-id-3').circleDiagram();
 });
