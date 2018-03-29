57f0316f59dfb723ff2bd611dd3f8afec39ff407
salehrabbi <33930388+salehrabbi@users.noreply.github.com>
2018-03-29 16:13:35 +0600

    pc version done

--- js/custom.js
+++ js/custom.js
@@ -8,9 +8,48 @@ $("#play-list-class").click(function () {
     $(".play-list").toggleClass("active");
 });
 
+$(".write-comment").click(function () {
+    $(this).toggleClass("active");
+    $(".your-comment").toggleClass("active");
+});
+
+$(".your-comment .cencel").click(function () {
+    $(".write-comment").removeClass("active");
+    $(".your-comment").removeClass("active");
+});
+
 $(function() {
 
 	$('#diagram-id-1').circleDiagram();
 	$('#diagram-id-2').circleDiagram();
 	$('#diagram-id-3').circleDiagram();
 });
+
+
+$('.owl-carousel-1').owlCarousel({
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    rewind: true,
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+
+    responsive: {
+        0: {
+            items: 2,
+            nav: false,
+            loop: false
+        },
+        480: {
+            items: 3,
+            nav: false,
+            loop: false
+        },
+        576: {
+            items: 3,
+        }
+    }
+})
